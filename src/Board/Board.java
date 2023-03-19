1a1fe71671dc63f6b0c214199f65bb6a27a6d236
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 20:06:02 +0100

    Created functions to set Pieces on a generic board. This methods should be called by an input validation class. Also created a clearBoard function.

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,7 +1,25 @@
 package Board;
 
 public abstract class Board {
+    protected int maxColumn;
+    protected int maxRow;
     char[][] board;
     char blankPiece;
 
+
+    public void setPieceAt(char p, int column, int row) {
+        this.board[column][row] = p;
+    }
+
+    public char getPieceAt(int column, int row) {
+        return this.board[column][row];
+    }
+
+    public void clearBoard() {
+        for (int column = 0; column < this.maxColumn; column++) {
+            for (int row = 0; row < this.maxRow; row++) {
+                this.setPieceAt(this.blankPiece, column, row);
+            }
+        }
+    }
 }

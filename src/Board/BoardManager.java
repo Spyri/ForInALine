03f26a8e9e281d28e0e5d75c51273df84cda2eec
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 20:10:58 +0100

    Boardmanager got refactored. Also draws the board on the console

--- src/Board/BoardManager.java
+++ src/Board/BoardManager.java
@@ -1,4 +1,37 @@
 package Board;
 
 public class BoardManager {
+    public Board board;
+    private PieceOnFIALBoardSetter pieceSetter;
+
+    public BoardManager(Board board) {
+        this.board = board;
+        this.pieceSetter = new PieceOnFIALBoardSetter(this.board);
+    }
+
+    public boolean setPieceAt(char p, int row) {
+        return this.pieceSetter.setPieceAt(p, row);
+    }
+
+    public void clearBoard() {
+        this.board.clearBoard();
+    }
+
+    public void drawBoard() {
+        for (int column = 0; column < this.board.maxColumn; column++) {
+            System.out.print('|');
+            for (int row = 0; row < this.board.maxRow; row++) {
+                System.out.print(this.board.getPieceAt(column, row) + "|");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 2 * this.board.maxRow + 1; i++) {
+            System.out.print('-');
+        }
+        System.out.println();
+        for (int row = 0; row < this.board.maxRow; row++) {
+            System.out.print(" " + row) ;
+        }
+        System.out.println("\n");
+    }
 }

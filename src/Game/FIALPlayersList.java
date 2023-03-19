6bba199e779a88ef0a16366715376cd82a6da158
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 21:02:43 +0100

    Fixed an issue that made impossible to choose a Symbol.

--- src/Game/FIALPlayersList.java
+++ src/Game/FIALPlayersList.java
@@ -11,7 +11,9 @@ public class FIALPlayersList {
     private char alreadyChosenSymbol;
 
     public FIALPlayersList() {
+        System.out.println("--- Player 1: Please type in your name and the symbol that you want to use. ---");
         Player player1 = createHumanPlayer();
+        System.out.println("--- Player 2: Please type in your name and the symbol that you want to use. ---");
         Player player2 = createHumanPlayer();
         this.players = new Player[]{player1, player2};
     }
@@ -24,7 +26,7 @@ public class FIALPlayersList {
         while (true) {
             System.out.print("Type in the Symbol you want to use: ");
             String c = scanner.next();
-            if (c.length() != 1) {
+            if (c.length() == 1) {
                 char chosenChar = c.charAt(0);
                 if (chosenChar == ' ') {
                     System.out.println("This Symbol is already taken. Try again.");

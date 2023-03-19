995744b9952f85ba36536a0d7d63473d2f1fc6dc
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 20:50:01 +0100

    Created a PlayersManager that manages the players of a FIAL game.

---
+++ src/Players/FIALPlayersManager.java
@@ -0,0 +1,10 @@
+package Players;
+
+import Game.FIALPlayersList;
+
+public class FIALPlayersManager {
+    private FIALPlayersList players;
+    public FIALPlayersManager(FIALPlayersList players) {
+        this.players = players;
+    }
+}

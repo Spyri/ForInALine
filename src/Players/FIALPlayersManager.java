5854a21ed34dafd4ad0e7e27c6122a66c6e49dec
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 21:09:05 +0100

    Implemented change player function

--- src/Players/FIALPlayersManager.java
+++ src/Players/FIALPlayersManager.java
@@ -3,8 +3,20 @@ package Players;
 import Game.FIALPlayersList;
 
 public class FIALPlayersManager {
-    private FIALPlayersList players;
+    private final FIALPlayersList players;
+    public Player currentPlayer;
+    public int round;
+
     public FIALPlayersManager(FIALPlayersList players) {
         this.players = players;
+        round = 0;
+        currentPlayer = players.players[round];
+    }
+
+
+
+    public void changePlayer() {
+        round++;
+        currentPlayer = players.players[round % 2];
     }
 }

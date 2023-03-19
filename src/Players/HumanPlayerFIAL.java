d7282133cacfea45f78ba06daa1dc50cbc6ac4d0
Spyros <spyropoulosspyros@hotmail.com>
2023-03-19 20:10:07 +0100

    Created A HumanPlayer class

---
+++ src/Players/HumanPlayerFIAL.java
@@ -0,0 +1,14 @@
+package Players;
+
+public class HumanPlayerFIAL extends Player{
+    public HumanPlayerFIAL(String name, char pieceSymbol){
+        this.name = name;
+        this.pieceSymbol = pieceSymbol;
+    }
+
+
+    @Override
+    public void executeMove() {
+
+    }
+}
